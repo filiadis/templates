b50b47f9c8b7d88e8c8be486504085c021615594
filiadis <iliadis.fil@gmail.com>
2025-12-19 17:07:53 +0200

    jjj

--- react_openlayers/v1/src/App.tsx
+++ react_openlayers/v1/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import OpenLayersMap from "./components/OpenLayersMap";
 import Foo from "./components/Foo";
 
 function App() {
   return (
     <>
       <Foo />
-      <OpenLayersMap />;
     </>
   );
 }
--- react_openlayers/v1/src/components/Foo.tsx
+++ react_openlayers/v1/src/components/Foo.tsx
@@ -1,11 +1,10 @@
-const Foo = () => {
+export default function Foo() {
   {
     return (
       <>
         <div>Hello</div>
+        <div className="title">ddd</div>
       </>
     );
   }
-};
-
-export default Foo;
+}
