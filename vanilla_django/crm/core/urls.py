ba9deb5033b708d078ec79d7b309c23a85096f96
filiadis <iliadis.fil@gmail.com>
2025-12-26 17:22:15 +0200

    gg

--- vanilla_django/crm/core/urls.py
+++ vanilla_django/crm/core/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", home_view, name="home"),
+    path("login/", login_view, name="login"),
+
 ]
